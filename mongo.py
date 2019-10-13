093bdd21aa418f4ea1adbd6efc5856aceb5ed181
Marco <marco.bellia0@gmail.com>
2019-10-13 20:05:49 +0200

    add measure - utils.py created

--- mongo.py
+++ mongo.py
@@ -1,17 +1,39 @@
 from pymongo import MongoClient
+from datetime import datetime
 
 class Mongo:
     def __init__(self):
         self.client = MongoClient('localhost', 27017)
         self.db = self.client.pymongo_test
-        posts = self.db.posts
-        post_data = {
-            'title': 'Python and MongoDB',
-            'content': 'PyMongo is fun, you guys',
-            'author': 'Scott'
+
+    def getDbs(self):
+        print( self.client.list_database_names())
+
+    def getDB(self):
+        return self.db
+        
+
+    def add_measure(self,temperature,humidity,brightness,img_url=None):
+        today = datetime.today()
+        measures = self.db.measures
+        measure_data = {
+            'date': today,
+            'temperature': temperature,
+            'humidity': humidity,
+            'brightness': brightness,
+            'img_url': img_url
+            
         }   
-        result = posts.insert_one(post_data)
-        print('One post: {0}'.format(result.inserted_id))
+        result = measures.insert_one(measure_data)
+        print('measure added : {0}'.format(result.inserted_id))
+
+    def get_measure(self,date_from=None,date_to=None):
+        
 
 
+
+m=Mongo()
+print(m.getDbs)
+print(m.getDB().list_collection_names())
+m.add_measure(20,74.6,12)
 
---
+++ utils.py
@@ -0,0 +1,3 @@
+class Utils:
+    def create_date(y,m,d,h=0,m=0,s=1):
+        
