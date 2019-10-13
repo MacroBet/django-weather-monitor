bd971a61a3dac9e204989061fceb36c9ab1923ed
Marco <marco.bellia0@gmail.com>
2019-10-13 20:24:54 +0200

    get_measures

--- mongo.py
+++ mongo.py
@@ -1,10 +1,12 @@
 from pymongo import MongoClient
 from datetime import datetime
+from pprint import pprint
 
 class Mongo:
     def __init__(self):
         self.client = MongoClient('localhost', 27017)
         self.db = self.client.pymongo_test
+        self.debug=True
 
     def getDbs(self):
         print( self.client.list_database_names())
@@ -17,7 +19,7 @@ class Mongo:
         today = datetime.today()
         measures = self.db.measures
         measure_data = {
-            'date': today,
+            'date': today.now().isoformat(),
             'temperature': temperature,
             'humidity': humidity,
             'brightness': brightness,
@@ -27,7 +29,14 @@ class Mongo:
         result = measures.insert_one(measure_data)
         print('measure added : {0}'.format(result.inserted_id))
 
-    def get_measure(self,date_from=None,date_to=None):
+    def delete_all_measures(self):
+        if(self.debug):
+            x = self.db["measures"].delete_many({})
+
+    def get_measures(self,date_from=None,date_to=None):
+        meausures = self.db["measures"]
+        return meausures.find({}).sort("date",1)
+        
         
 
 
@@ -35,5 +44,12 @@ class Mongo:
 m=Mongo()
 print(m.getDbs)
 print(m.getDB().list_collection_names())
-m.add_measure(20,74.6,12)
+#m.add_measure(20,74.6,12)
+
+
+cursor = m.get_measures()
+
+for document in cursor: 
+    pprint(document)
+
 
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 class Utils:
     def create_date(y,m,d,h=0,m=0,s=1):
-        
+        return datetime(y, m, d, h, m, s)        
